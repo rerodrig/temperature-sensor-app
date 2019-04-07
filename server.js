c05a5a4f37f0f330cc7ef43ec123615ba2e73038
Renato Rodrigues <renatorodriguesnet@gmail.com>
2019-04-07 20:55:14 -0300

    Serve index.html of www folder

--- server.js
+++ server.js
@@ -1,22 +1,5 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 
-
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
-app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
