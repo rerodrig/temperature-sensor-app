6051037fb1326401ad9ba5ebd42963d07537c637
Renato Rodrigues <renatorodriguesnet@gmail.com>
2019-04-07 19:17:47 -0300

    Removing unnecessary module declaration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'measurements', loadChildren: './components/measurements/measurements.module#MeasurementsPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
 ];
 
 @NgModule({
