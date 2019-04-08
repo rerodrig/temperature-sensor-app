3b5ffbaf1efdf3567260c7efb06482e37a21b0d4
Renato Rodrigues <renatorodriguesnet@gmail.com>
2019-04-07 21:00:20 -0300

    Forcing redirect to https in server.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
