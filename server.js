e4924460fbab74710087ad12f0e82b949afd84b7
Renato Rodrigues <renatorodriguesnet@gmail.com>
2019-04-07 21:11:34 -0300

    Change index.html path in server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const forceSSL = function() {
 }
 
 app.use(forceSSL());
-app.use(express.static('www'));
+app.use(express.static(__dirname + '/www'));
 app.set('port', process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('www/index.html'));
+  res.sendFile(path.join(__dirname + '/www/index.html'));
 });
